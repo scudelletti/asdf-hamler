f601b0e30d00ea435eae08555f317ff1085cf69e
Diogo Scudelletti <diogo@scudelletti.com>
2021-03-09 10:07:25 +0100

    Retrieve versions based on tags without v prefix

--- lib/utils.bash
+++ lib/utils.bash
@@ -30,8 +30,7 @@ sort_versions() {
 
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- | grep v |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    grep -o 'refs/tags/.*' | cut -d/ -f3- | grep -v v
 }
 
 list_all_versions() {
